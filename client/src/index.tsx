20eda45e1506c599c12ac052d6a40daff89add1a
nghiatd <toduynghia@gmail.com>
2021-01-30 11:35:51 +0700

    Add client project

---
+++ client/src/index.tsx
@@ -0,0 +1,65 @@
+/**
+ * index.tsx
+ *
+ * This is the entry file for the application, only setup and boilerplate
+ * code.
+ */
+
+import { App } from 'app';
+import FontFaceObserver from 'fontfaceobserver';
+import * as React from 'react';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import * as ReactDOM from 'react-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
+import reportWebVitals from 'reportWebVitals';
+// Use consistent styling
+import 'sanitize.css/sanitize.css';
+import { configureAppStore } from 'store/configureStore';
+import { ThemeProvider } from 'styles/theme/ThemeProvider';
+// Initialize languages
+import './locales/i18n';
+import * as serviceWorker from './serviceWorker';
+
+// Observe loading of Inter (to remove 'Inter', remove the <link> tag in
+// the index.html file and this observer)
+const openSansObserver = new FontFaceObserver('Inter', {});
+
+// When Inter is loaded, add a font-family using Inter to the body
+openSansObserver.load().then(() => {
+  document.body.classList.add('fontLoaded');
+});
+
+const store = configureAppStore();
+const MOUNT_NODE = document.getElementById('root') as HTMLElement;
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider>
+      <HelmetProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </HelmetProvider>
+    </ThemeProvider>
+  </Provider>,
+  MOUNT_NODE,
+);
+
+// Hot reloadable translation json files
+if (module.hot) {
+  module.hot.accept(['./locales/i18n'], () => {
+    // No need to render the App again because i18next works with the hooks
+  });
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
